defc4fb5d1f1ee25674db12eaa0990328e989020
akthrms <akt.hrms@gmail.com>
2021-08-14 14:39:51 +0900

    update

--- src/cljls/core.clj
+++ src/cljls/core.clj
@@ -1,1 +1,9 @@
-(ns cljls.core)
+(ns cljls.core
+  (:import [java.io File]))
+
+(defn list-file-name
+  [path]
+  (->> (.listFiles (File. ^String path))
+       (filter #(not (.isHidden %)))
+       (sort-by #(.toString %))
+       (map #(.getName %))))
